c070a2769a69ae4f913f1d66ba838bfc2fa0dc52
Diego Escobar <diego.developer@yahoo.com>
2023-08-02 23:21:34 +0300

    adding auth method

--- netlify/functions/callback.ts
+++ netlify/functions/callback.ts
@@ -1,17 +1,46 @@
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-import { User } from "./db/models/User";
+import type { Handler, HandlerEvent } from "@netlify/functions";
+import axios from "axios";
 
+const handler: Handler = async (event: HandlerEvent) => {
 
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  const queryStringParameters = event?.headers?.queryStringParameters;
+  const code_verifier = process.env.TWITTER_CLIENT_CODE;
+  const client_id = process.env.TWITTER_CLIENT_ID
+  const redirect_uri = process.env.REDIRECT_URL
 
-  const users = await User.findAll();
-  console.log({ users });
+  const { code } = queryStringParameters as unknown as { code: string };
 
-  console.log({ event, context })
+  if (!code) {
     return {
-        statusCode: 200,
-        body: JSON.stringify({users}),
-      };
+      statusCode: 400,
+      body: 'No code provided'
+    }
+  }
+
+  // get access token from code
+  const response = await axios.post('https://api.twitter.com/2/oauth2/token', {}, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    params: {
+      grant_type: 'authorization_code',
+      code,
+      client_id,
+      code_verifier,
+      redirect_uri
+    }
+  });
+
+  console.log({ response });
+
+  // redirect to /
+  return {
+    statusCode: 302,
+    headers: {
+      Location: '/'
+    },
+
+  }
 };
 
 export { handler };
---
+++ netlify/functions/services/auth.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+const scopesArray = ["","tweet.write"]
+const scope = encodeURIComponent(scopesArray.join(" "))
+
+const prepareAuthUrl = (redirect_uri) => {
+    const client_id = process.env.TWITTER_CLIENT_ID || "Wk5oRW93azZFcWltU2F2eVBUeTM6MTpjaQ";
+    const code = process.env.TWITTER_CLIENT_CODE || "Wk5oRW93azZFcWltU2F2eVBUeTM6MTpjaQ:Ud0HI1h_zG1F813HOhZoYznC8cEO2tx-gmz9C_5Ww4nE59qOB1";
+    
+    return `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=state&code_challenge=${code}e&code_challenge_method=plain`
+}
+
+const url = prepareAuthUrl("https://x-plus.netlify.app/.netlify/functions/callback")
+
+console.log({ url })
