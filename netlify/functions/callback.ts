2644a098663aaf535726d1a3d0cff150929025ae
Diego Escobar <diego.developer@yahoo.com>
2023-08-03 02:59:24 +0300

    upp

--- netlify/functions/callback.ts
+++ netlify/functions/callback.ts
@@ -6,7 +6,10 @@ const handler: Handler = async (event: HandlerEvent) => {
   try {
     const { code, state } = event?.queryStringParameters || {};
 
-    if (!code || state !== process.env.TWITTER_STATE) throw new Error("State isn't matching");
+    if (!code || state !== process.env.TWITTER_STATE) {
+      console.log({ code, state, env: process.env.TWITTER_STATE })
+      throw new Error("State isn't matching");
+    }
 
     console.log({ code });
 
