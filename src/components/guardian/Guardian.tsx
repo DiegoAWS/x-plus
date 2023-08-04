0e15f8d1102b1fd82b6c38b7c8d112f8736020cb
Diego Escobar <diego.developer@yahoo.com>
2023-08-05 02:41:18 +0300

    nav home

--- src/components/guardian/Guardian.tsx
+++ src/components/guardian/Guardian.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useMainContext from "../../contexts/useMainContext";
 import { Navigate } from "react-router-dom";
-import { LOGIN_PATH } from "../../router";
+import { HOME_PATH } from "../../router";
 
 type Props = {
     children?: React.ReactNode;
@@ -11,7 +11,7 @@ type Props = {
 function Guardian({ children, publicComponent, privateComponent }: Props) {
   const { twitterToken } = useMainContext();
 
-  return  twitterToken ? (privateComponent|| children || null) : publicComponent || <Navigate to={LOGIN_PATH} />;
+  return  twitterToken ? (privateComponent|| children || null) : publicComponent || <Navigate to={HOME_PATH} />;
   
 }
 
