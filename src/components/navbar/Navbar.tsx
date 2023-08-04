2b213c549e059ad526d4a0b1b63988f3e78e4d06
Diego Escobar <diego.developer@yahoo.com>
2023-08-05 02:13:50 +0300

    UP

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Button, Row, Typography } from "antd";
+import useMainContext from "../../contexts/useMainContext";
+import "./Navbar.scss";
+import Logo from "../../assets/Logo";
+
+function Navbar() {
+  const { twitterToken, logout } = useMainContext();
+  const { me } = twitterToken as {
+    me: {
+      data: {
+        name: string;
+      };
+    };
+  };
+  console.log(twitterToken);
+  return (
+    <Row className="navbarWrapper" justify="space-between" align="middle">
+      <Row align="middle">
+        <Logo />
+        <Typography.Text>{me?.data?.name}</Typography.Text>
+      </Row>
+
+      <Button type="default" danger onClick={logout}>
+        Logout
+      </Button>
+    </Row>
+  );
+}
+
+export default Navbar;
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,28 @@
-import { Button, Typography } from "antd";
+import { Card, Typography } from "antd";
+import "./Dashboard.scss";
+import Navbar from "../../components/navbar/Navbar";
 import useMainContext from "../../contexts/useMainContext";
 
 function Dashboard() {
-  const { twitterToken, logout } = useMainContext();
-  console.log(twitterToken);
-  return <div>
-
-    <Typography.Title level={1}>Dashboard</Typography.Title>
-
-<Button type="primary" danger onClick={logout}>Logout</Button>
-  </div>;
+  const { isDarkTheme } = useMainContext();
+  return (
+    <div className="dashboardWrapper" id={isDarkTheme ? "dark" : undefined}>
+      <Navbar />
+      <Typography.Title level={2} className="title">Dashboard</Typography.Title>
+      <div className="dashboardContent">
+      
+      
+{
+  new Array(100).fill(0).map((_, index) => (
+    <Card title="Card title" key ={index}>
+        <img src={`https://picsum.photos/seed/${index+1}/600/500`} alt="profile" />
+        </Card>
+  ))
+}
+      
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
