8615b2070b91c4e23685bd0810e5077724fa61a3
Diego Escobar <diego.developer@yahoo.com>
2023-08-06 17:44:12 +0300

    send

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,30 @@
-import { Card } from "antd";
+import { Button, Card, Divider, Input } from "antd";
 import "./Dashboard.scss";
+import { SendOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 function Dashboard() {
-
+const [twittArea, setTwittArea] = useState('')
 
   return (
     <>
       <Card className="cardWrapper">
-        <h1>Dashboard</h1>
+        <Input.TextArea 
+        className="twittArea"
+        rows={4} 
+        showCount
+        value={twittArea}
+        onChange={(e) => setTwittArea(e.target.value)}
+        maxLength={280}
+        autoSize={{ minRows: 4, maxRows: 8}}
+        />
+        <Divider />
+        <Button icon={
+          <SendOutlined />
+        }
+        loading={!false}
+        disabled={!twittArea}
+        />
       </Card>
     </>
   );
