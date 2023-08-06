f36fb37f9a44870ff698aaa28c4d85c4fda1d2c0
Diego Escobar <diego.developer@yahoo.com>
2023-08-07 02:17:50 +0300

    up

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Button, Grid, Layout, Typography, theme } from "antd";
 
-import { DingtalkOutlined, LogoutOutlined } from "@ant-design/icons";
+import { DingtalkOutlined } from "@ant-design/icons";
 import "./Navbar.scss";
 import netlifyIdentity from "../../services/niw";
+import LogOutIcon from "../../assets/svg/LogOutIcon";
+
 function Navbar() {
 
   const {
@@ -10,6 +12,11 @@ function Navbar() {
   } = theme.useToken();
 
   const { sm } = Grid.useBreakpoint();
+
+  const logout = () => {
+    netlifyIdentity.logout();
+    // window.location.reload(); // HARD refresh
+  };
 
   return (
     <Layout.Header
@@ -25,16 +32,16 @@ function Navbar() {
             color: "red",
           }}
         />
-        
-          {sm && <Typography.Text ellipsis className="brandText">
-          Brand Name
-          </Typography.Text>}
-       
+
+        {sm && (
+          <Typography.Text ellipsis className="brandText">
+            Brand Name
+          </Typography.Text>
+        )}
       </div>
-      <Button shape="round" onClick={()=>{
-        netlifyIdentity.logout();
-      }} icon={<LogoutOutlined />}>
-        Logout
+      <Button shape="round" onClick={logout}>
+        {netlifyIdentity.currentUser()?.email}
+        <LogOutIcon className="logoutIcon" />
       </Button>
     </Layout.Header>
   );
