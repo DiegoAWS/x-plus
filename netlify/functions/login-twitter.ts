bd89bcead9112e4aa0edf061cf7b34b91549e541
Diego Escobar <diego.developer@yahoo.com>
2023-08-12 16:57:35 +0300

    upp

--- netlify/functions/login-twitter.ts
+++ netlify/functions/login-twitter.ts
@@ -14,6 +14,7 @@ const handler: Handler = async (
   const user = context.clientContext?.user;
   const adminToken = context.clientContext?.identity?.token;
 
+  console.log({ client: context.clientContext, user, adminToken })
   const { code, companyName } = JSON.parse(event?.body || "{}");
 
   if (!code || !companyName) {
